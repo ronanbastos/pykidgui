ab223256b6337584e62cb6b09546e9280be580c2
Ronan Bastos <23327501+ronanbastos@users.noreply.github.com>
2023-03-01 20:47:26 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,79 +1,160 @@
-name = "pykidgui"
-import pykidgui as pk
+from pykidgui import *
+
 
 '''
-print(pk.version)
-pk.text_title= "Loja"
-pk.window_geometry = "550x550"
-pk.window_name("home")
+# onclick ex
+def on_click(button_name, text):
+    print(button_name + " clicado com o texto: " + text)
 
-pk.add_build_map("checkbox")
-pk.new_checkbox("check1")
+my_gui = Gui("Janela")
+my_gui.add_label("label1", "Digite algo abaixo:")
+my_gui.add_button("button1", "OK 1", on_click)
+my_gui.add_button("button2", "OK 2", on_click)
 
-pk.add_build_map("checkbox:LEFT")
-pk.new_checkbox("check2")
-pk.add_build_map("checkbox:left")
-pk.new_checkbox("check clique3")
-
-
-
-pk.add_build_map("label")
-pk.new_label("Loja teste")
-
-pk.add_build_map("on_click")
-pk.new_on_click_text = "cadastar produtos"
-pk.new_on_click_var=te.j
-
-pk.add_build_map("label")
-
-
-
-pk.add_build_map("button")
-pk.new_button("sair")
-
-
-
-print(pk.build_map)
-pk.star()
+my_gui.mainloop()
 '''
-def msg():
-     pk.new_message_erro("testando o sistem de mansagens!!")
-def k():
-    x = 500
-    y = 500
-    result = x+y
-    try:
-        return result
-    except:
-        pk.new_message_title("Erro","Erro de script")
+'''
+# entry ex
+my_gui = Gui("Janela")
+my_entry = my_gui.add_entry("entry1")
 
-
-def x():
-    x = 500
-    y = 500
-    result = x+y
-    msg="variavel msg"
-    pk.new_message_title("resultado",result)
+def on_click(button_name, text):
+    entry_value = my_gui.get_entry_value(my_entry)
+    print(f"Valor digitado: {entry_value}")
     
+my_gui.add_button("button1", "Clique aqui", on_click)
+my_gui.mainloop()
 
-pk.new_label("button 1")
-pk.add_build_map("label")
+'''
+#listbox
 
-pk.new_click_text("message teste") 
-pk.new_on_click(msg)
-pk.add_build_map("on_click")
-
-
-pk.new_label("button 2")
-pk.add_build_map("label")
+'''
+my_gui = Gui("Janela")
+my_gui.add_listbox("opcoes", ["opção 1", "opção 2", "opção 3"])
 
 
-pk.new_click_text("Resultado") 
-pk.new_on_click(x)
-pk.add_build_map("on_click")
+selected_index = my_gui.widgets["opcoes"].curselection()
+if selected_index:
+    selected_option = my_gui.widgets["opcoes"].get(selected_index)
+    print(selected_option)
+else:
+    print("Nenhuma opção selecionada.")
 
-pk.text_title="test title"
-pk.window_geometry="200x200"
 
+my_gui.mainloop()
 
-pk.gui()
+'''
+'''
+# area de texto
+
+my_gui = Gui("Janela")
+my_gui.add_text_area("texto1")
+my_gui.mainloop()
+'''
+'''
+#processer Bar
+
+my_gui = Gui("Janela")
+progress_bar_var = my_gui.add_progress_bar("barra_de_progresso")
+progress_bar_var.set(50) 
+'''
+'''
+map_data = [
+    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#333", "#333", "#333", "#333", "#333", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
+]
+
+root = tk.Tk()
+canvas_map = CanvasMap(root, map_data)
+canvas_map.pack()
+root.mainloop()
+'''
+
+'''
+from pykidgui import *
+
+my_gui = Gui("Janela")
+
+# Adicionar um canvas com uma largura e altura definidas
+canvas = my_gui.add_canvas("canvas", width=300, height=300)
+
+# Criar um mapa para uma linha que começa na posição (50, 50) e termina na posição (250, 250)
+line_map = {'shape': 'line', 'coords': [50, 50, 250, 250], 'fill': 'black', 'width': 2}
+line_map1 = {'shape': 'line', 'coords': [100,150, 250, 250], 'fill': 'red', 'width': 2}
+# Adicionar a linha ao canvas
+my_gui.add_canvas_items("canvas", [line_map])
+my_gui.add_canvas_items("canvas", [line_map1])
+
+# Executar o loop principal da GUI
+my_gui.mainloop()
+
+'''
+
+'''
+def on_checkbox_clicked(checked):
+    print("Checkbox marcado" if checked else "Checkbox desmarcado")
+
+my_gui = Gui("Exemplo de Checkbox", "300x200")
+my_gui.add_checkbox("Marque aqui", on_checkbox_clicked)
+my_gui.add_checkbox("Marque aqui", on_checkbox_clicked)
+my_gui.mainloop()
+'''
+'''
+checkbox_values = []
+
+my_gui = Gui("Exemplo de Checkbox", "300x200")
+
+def on_checkbox_clicked(value):
+    checkbox_values.append(value)
+    print(checkbox_values)
+
+my_gui.add_checkbox("Checkbox 1", on_checkbox_clicked)
+my_gui.add_checkbox("Checkbox 2", on_checkbox_clicked)
+my_gui.add_checkbox("Checkbox 3", on_checkbox_clicked)
+
+my_gui.mainloop()
+
+'''
+'''
+def minha_funcao_callback(value):
+ 
+    print(value)
+    
+gui = Gui("Minha janela", "400x300")
+gui.add_spinbox("Idade", 0, 100,minha_funcao_callback)
+gui.mainloop()
+
+'''
+'''
+def minha_funcao1():
+ 
+    print("novo arquivo")
+
+def minha_funcao2():
+ 
+    print("abrir arquivo")
+
+def minha_funcao3():
+ 
+    print("sair")
+            
+gui = Gui("Minha janela", "400x400")
+gui.add_menu("Arquivo", [("Novo", minha_funcao1), ("Abrir", minha_funcao2), "-", ("Sair", minha_funcao3)])
+gui.add_menu("Arquivo2", [("Novo2", minha_funcao1), ("Abrir2", minha_funcao2), "-", ("Sair2", minha_funcao3)])
+gui.mainloop()
+'''
+'''
+gui = Gui("Minha janela", "500x500")
+img1 = gui.add_image("test.png")
+img1.set_position(15,100)
+gui.mainloop()
+'''
+
+gui = Gui("Minha janela", "500x500")
+gui.add_label("Hello, world!")
+scrollbar = gui.add_scrollbar()
+gui.mainloop()
