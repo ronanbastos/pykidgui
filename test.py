9e45986db1d02b21f29670b44182b37871171be5
Ronan Bastos <23327501+ronanbastos@users.noreply.github.com>
2023-03-01 22:18:24 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,52 @@
 from pykidgui import *
 
+from pykidgui import *
+import tkinter as tk
 
-'''
+
+map_data = [
+    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#333", "#333", "#333", "#333", "#333", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
+    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
+]
+
+root = tk.Tk()
+canvas_map = Canvas_Map(root, map_data)
+canvas_map.pack()
+root.mainloop()
+
+
 # onclick ex
-def on_click(button_name, text):
-    print(button_name + " clicado com o texto: " + text)
-
-my_gui = Gui("Janela")
-my_gui.add_label("label1", "Digite algo abaixo:")
-my_gui.add_button("button1",  on_click , "ok 1")
-my_gui.add_button("button2", on_click , "OK 2")
-
-my_gui.mainloop()
 '''
+def hello_world():
+    print("Hello, World!")
+
+my_gui = Gui("Janela","500x500")
+
+# Adiciona um botão à janela principal
+my_gui.add_button("hello_button", hello_world, text="Say Hello!")
 '''
+
+
 # entry ex
-my_gui = Gui("Janela")
+my_gui = Gui("Janela","500x500")
 my_entry = my_gui.add_entry("entry1")
 
-def on_click(button_name, text):
+def on_click():
     entry_value = my_gui.get_entry_value(my_entry)
     print(f"Valor digitado: {entry_value}")
     
-my_gui.add_button("button1", "Clique aqui", on_click)
+my_gui.add_button("button1",on_click, "Clique aqui")
 my_gui.mainloop()
 
-'''
+
 #listbox
 
-'''
-my_gui = Gui("Janela")
+
+my_gui = Gui("Janela","500x500")
 my_gui.add_listbox("opcoes", ["opção 1", "opção 2", "opção 3"])
 
 
@@ -43,41 +60,18 @@ else:
 
 my_gui.mainloop()
 
-'''
-'''
 # area de texto
 
-my_gui = Gui("Janela")
+my_gui = Gui("Janela","500x500")
 my_gui.add_text_area("texto1")
 my_gui.mainloop()
-'''
-'''
+
 #processer Bar
 
-my_gui = Gui("Janela")
-progress_bar_var = my_gui.add_progress_bar("barra_de_progresso")
-progress_bar_var.set(50) 
-'''
-'''
-map_data = [
-    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
-    ["#333", "#FFF", "#FFF", "#FFF", "#333", "#333", "#333", "#333", "#333", "#333"],
-    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
-    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
-    ["#333", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#FFF", "#333"],
-    ["#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333", "#333"],
-]
 
-root = tk.Tk()
-canvas_map = CanvasMap(root, map_data)
-canvas_map.pack()
-root.mainloop()
-'''
-
-'''
 from pykidgui import *
 
-my_gui = Gui("Janela")
+my_gui = Gui("Janela","500x500")
 
 # Adicionar um canvas com uma largura e altura definidas
 canvas = my_gui.add_canvas("canvas", width=300, height=300)
@@ -92,9 +86,7 @@ my_gui.add_canvas_items("canvas", [line_map1])
 # Executar o loop principal da GUI
 my_gui.mainloop()
 
-'''
 
-'''
 def on_checkbox_clicked(checked):
     print("Checkbox marcado" if checked else "Checkbox desmarcado")
 
@@ -102,8 +94,7 @@ my_gui = Gui("Exemplo de Checkbox", "300x200")
 my_gui.add_checkbox("Marque aqui", on_checkbox_clicked)
 my_gui.add_checkbox("Marque aqui", on_checkbox_clicked)
 my_gui.mainloop()
-'''
-'''
+
 checkbox_values = []
 
 my_gui = Gui("Exemplo de Checkbox", "300x200")
@@ -118,8 +109,7 @@ my_gui.add_checkbox("Checkbox 3", on_checkbox_clicked)
 
 my_gui.mainloop()
 
-'''
-'''
+
 def minha_funcao_callback(value):
  
     print(value)
@@ -128,8 +118,8 @@ gui = Gui("Minha janela", "400x300")
 gui.add_spinbox("Idade", 0, 100,minha_funcao_callback)
 gui.mainloop()
 
-'''
-'''
+
+
 def minha_funcao1():
  
     print("novo arquivo")
@@ -146,15 +136,14 @@ gui = Gui("Minha janela", "400x400")
 gui.add_menu("Arquivo", [("Novo", minha_funcao1), ("Abrir", minha_funcao2), "-", ("Sair", minha_funcao3)])
 gui.add_menu("Arquivo2", [("Novo2", minha_funcao1), ("Abrir2", minha_funcao2), "-", ("Sair2", minha_funcao3)])
 gui.mainloop()
-'''
-'''
+
 gui = Gui("Minha janela", "500x500")
 img1 = gui.add_image("test.png")
 img1.set_position(15,100)
 gui.mainloop()
-'''
 
 gui = Gui("Minha janela", "500x500")
 gui.add_label("Hello, world!")
 scrollbar = gui.add_scrollbar()
 gui.mainloop()
+
